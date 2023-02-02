d520723540a726d03fa9b0680c939b12455a833b
JianWang2016 <jianwang2010@gmail.com>
2023-02-02 02:54:24 -0500

    Encounter the same aws-exports issue, not sure if this has something to do with the stage backend

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -16,8 +16,12 @@ export function Footer() {
             <div className="flex items-center text-gray-900">
               <Logomark className="h-10 w-10 flex-none fill-cyan-500" />
               <div className="ml-4">
-                <p className="text-base text-gray-600 font-semibold">Marathon Ranking</p>
-                <p className="mt-1 text-sm">It is never too late to dream big!</p>
+                <p className="text-base font-semibold text-gray-600">
+                  Marathon Ranking
+                </p>
+                <p className="mt-1 text-sm">
+                  Run a marathon and change your life!
+                </p>
               </div>
             </div>
           </div>
